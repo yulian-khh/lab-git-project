832ea82de617ea35d90109d3c216ca26a7668bcf
yulian-khh <yulian.kh5002@gmail.com>
2025-01-20 09:47:14 +0300

    Add new_feature function

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,10 @@ def multiply_numbers(a, b):
 def add_numbers(a, b):
     # Функция для сложения чисел
     return a + b
+
+def some_function():
+    # Изменение, которое не нужно коммитить
+    pass
+
+def new_feature():
+    return "This is a new feature"
